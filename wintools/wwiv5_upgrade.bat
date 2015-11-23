70d6948c3c8f5bae98f880e838b1165ec12d6784
TRI0N <tri0n@hotmail.com>
2015-11-22 17:25:06 -0800

    Added Windows 8 and 10 Compatibility
    No longer need to put the build number at the end of the batch file. It will prompt for your to input the batch number. You can now choose between 7-Zip and WinRAR. Prompt to start WWIV5TelnetServer after update and launches the latest changes into the default web browser.

--- wintools/wwiv5_upgrade.bat
+++ wintools/wwiv5_upgrade.bat
@@ -1,4 +1,4 @@
-@Echo Off
+@Echo OFF
 
 :: BatchGotAdmin
 :-------------------------------------
@@ -26,65 +26,138 @@ if '%errorlevel%' NEQ '0' (
 
 @Echo OFF
 
+:LAUNCH 
+set olddir=%CD%
+goto START
+
+:START
+REM CREATE UNIQUE FILE NAME FOR BACKUP
+set local 
+set year=%DATE:~-4,4% 
+set month=%DATE:~-7,2% 
+set day=%DATE:~-10,2% 
+set hour=%TIME:~0,2% 
+REM REMOVE HOUR LEADING ZEROS 
+if '%hour:~0,1%' == ' ' set hour=0%hour:~1,1% 
+set minute=%time:~3,2% 
+set seconds=%time:~6,2% 
+set timestamp=%year%%month%%day%%hour%%minute%%seconds%
+REM TRIM OUT SPACES
+set timestamp=%timestamp: =%
+
 REM This requires you have WGET You need the Binaries
 REM and the dependencies which can be found here:
 REM http://gnuwin32.sourceforge.net/packages/wget.htm
 
-REM This requires you have 7ZIP which can be found here:
+REM This requires you have 7-ZIP which can be found here:
 REM http://www.7-zip.org/download.html
 REM or modify to use your favorite compression utility.
 
-
-IF [%1]==[] GOTO USAGE
+IF [%1]==[] GOTO INPUTS
 IF [%1]==[?] GOTO USAGE
 IF [%1]==[-?] GOTO USAGE
 IF [%1]==[/?] GOTO USAGE
 
-:BEGIN
-set local
-REM Preparing Timestamp Information
-set year=%date:~10,4%
-set month=%date:~4,2%
-set day=%date:~7,2%
-set hour=%time:~0,2%
-REM Replace leading space with zero
-if “%hour:~0,1%” ==” ” set hour=0%hour:~1,1%
-set minute=%time:~3,2%
-set seconds=%time:~6,2%
-set timestamp=%year%%month%%day%%hour%%minute%%seconds%
-set backup_file=%userprofile%\documents\%timestamp%_wwiv.zip
+:INPUTS
+cls
+ECHO.
+ECHO *** MAKE SURE WWIV AND TELNET SERVER ARE CLOSED BEFORE PROCEEDING ***
+ECHO.
+ECHO.
+ECHO ENTER BUILD NUMBER FOR UPDATE
+ECHO.
+set /p build=Enter Build Number: 
 
-REM BAckup the current running version 
-c:
-cd \
+if %build% EQU '' GOTO INPUTS if NOT GOTO TOOL
 
-7z a -r %backup_file% c:\wwiv\*.*
+:TOOL
+cls
+ECHO.
+ECHO Select A COMPRESSION TOOL:
+ECHO ==========================
+ECHO.
+ECHO 1) 7-Zip
+ECHO 2) WinRAR
+ECHO 3) ABORT UDATE
+ECHO.
+set /p input=Select Option: 
 
-REM GET the New Build and upgrade the install
-c:
-cd %userprofile%\downloads\
-c:\tools\wget http://build.wwivbbs.org/job/WWIV/label=windows/%1/artifact/wwiv-build-win-%1.zip
-7z e -oc:\wwiv -y wwiv-build-win-%1.zip *.exe
-7z e -oc:\wwiv -y wwiv-build-win-%1.zip *.dll
-7z e -o%SystemRoot%\system32 -y wwiv-build-win-%1.zip *.dll
-GOTO DONE
+if %input% EQU 1 GOTO 7ZIP if NOT GOTO TOOL
+if %input% EQU 2 GOTO WINRAR if NOT GOTO TOOL
+if %input% EQU 3 GOTO ABORT if NOT GOTO TOOL
+if %input% GTR 3 GOTO TOOL
+
+:7ZIP
+REM BACKUP WWIV
+set backup_file1=%USERPROFILE%\documents\%timestamp%_wwiv.zip
+cd "%ProgramFiles%\7-Zip\"
+7z.exe a -r -y %backup_file1% C:\wwiv\*.*
+REM FETCH NEW WWIV UPDATE
+set build_file=http://build.wwivbbs.org/job/WWIV/%build%/label=windows/artifact/wwiv-build-win-%build%.zip
+cd %USERPROFILE%\downloads\
+"%ProgramFiles(x86)%\GnuWin32\bin\wget.exe" %build_file%
+REM EXTRACT FILES FOR UPDATE
+"%ProgramFiles%\7-Zip\7z.exe" e -oc:\wwiv -y wwiv-build-win-%build%.zip *.exe
+"%ProgramFiles%\7-Zip\7z.exe" e -oc:\wwiv -y wwiv-build-win-%build%.zip *.dll
+"%ProgramFiles%\7-Zip\7z.exe" e -o%SystemRoot%\system32 -y wwiv-build-win-%build%.zip *.dll
+ECHO.
+ECHO WWIV UDATE COMPLETE!
+GOTO RUNWWIV
+
+:WINRAR
+REM BACKUP WWIV
+set backup_file2=%USERPROFILE%\documents\%timestamp%_wwiv.rar
+cd "%ProgramFiles%\WinRAR\"
+Rar.exe a -r %backup_file2% C:\wwiv\*.*
+REM FETCH NEW WWIV UPDATE
+set build_file=http://build.wwivbbs.org/job/WWIV/%build%/label=windows/artifact/wwiv-build-win-%build%.zip
+cd %USERPROFILE%\downloads\
+"%ProgramFiles(x86)%\GnuWin32\bin\wget.exe" %build_file%
+REM EXTRACT FILES FOR UPDATE
+"%ProgramFiles%\WinRAR\WinRAR.exe" e -y wwiv-build-win-%build%.zip *.exe C:\wwiv
+"%ProgramFiles%\WinRAR\WinRAR.exe" e -y wwiv-build-win-%build%.zip *.dll C:\wwiv
+"%ProgramFiles%\WinRAR\WinRAR.exe" e -y wwiv-build-win-%build%.zip *.dll %SystemRoot%\system32
+ECHO.
+ECHO WWIV UDATE COMPLETE!
+GOTO RUNWWIV
 
 :USAGE
 ECHO How to Use WWIV 5.0 build to build wwiv5_upgrade.bat
 ECHO.
-ECHO     SYNTAX: wwiv5_upgrade.bat ####
-ECHO         ### is a build number
+ECHO     SYNTAX: wwiv5_upgrade.bat
 ECHO.
 ECHO     Paths and files names are based on your environment
-ECHO     Requires WGET and 7Zip. Links in the Comments.
+ECHO     Requires WGET and 7-Zip. Links in the Comments.
 ECHO.
 ECHO     Backup will be placed here: 
-ECHO         %userprofile%\documents\timestamp_wwiv.zip
+ECHO         %USERPROFILE%\documents\timestamp_wwiv.zip
 ECHO     New Build is expects to be found here: 
-ECHO         http://build.wwivbbs.org/job/WWIV/label=windows
-ECHO              /####/artifact/wwiv-build-win-####.zip
+ECHO         http://build.wwivbbs.org/job/WWIV/####/label=windows
+ECHO              /artifact/wwiv-build-win-####.zip
 ECHO.
 GOTO DONE
 
+:RUNWWIV
+ECHO.
+set /p run_wwiv="Launch WWIV Now? Press 1 for YES | 2 for NO: "
+ECHO.
+if %run_wwiv% EQU 1 GOTO LAUNCHWWIV in NOT GOTO RUNWWIV
+if %run_wwiv% EQU 2 GOTO CHANGES in NOT GOTO RUNWWIV
+
+:LAUNCHWWIV
+START C:\wwiv\WWIV5TelnetServer.exe
+GOTO CHANGES
+
+:CHANGES
+START http://build.wwivbbs.org/job/WWIV/%build%/changes
+GOTO DONE
+
+:ABORT
+ECHO.
+ECHO UPDATE ABORTED BY USER
+GOTO DONE
+
 :DONE
-pause
+cd "%olddir%"
+ECHO.
+TIMEOUT /T 30
